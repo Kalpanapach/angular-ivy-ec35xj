58401aebb9eb44ad6d536ebc7718b1ce8c14280a
Kalpanapach <122963689+Kalpanapach@users.noreply.github.com>
2023-01-18 16:43:15 +0530

    4 files modified, 6 added

---
+++ src/app/access/access.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-access',
+  template: '<p>  access works!  </p>',
+  styles: ['  .custom { color :red}  '],
+})
+export class AccessComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
